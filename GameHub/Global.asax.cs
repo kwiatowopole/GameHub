c8800e8c1a09dc9b54a40de88bbd9edbacc90a00
iaros <iaroslavkviatkovskii@gmail.com>
2025-06-14 22:38:11 +0200

    Ready for Online

--- GameHub/Global.asax.cs
+++ GameHub/Global.asax.cs
@@ -1,5 +1,7 @@
+using GameHub.Models;
 using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -12,6 +14,7 @@ namespace GameHub
     {
         protected void Application_Start()
         {
+            Database.SetInitializer(new MigrateDatabaseToLatestVersion<AppDBContext, Migrations.Configuration>());
             AreaRegistration.RegisterAllAreas();
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
